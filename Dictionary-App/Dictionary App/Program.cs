cd9f644d454a2c362371b8f06c3015c9b8e5d8fc
DelphiicOracle <49090128+delphiioracle@users.noreply.github.com>
2023-09-03 19:01:20 +0100

    Implemented Add

--- Dictionary-App/Dictionary App/Program.cs
+++ Dictionary-App/Dictionary App/Program.cs
@@ -4,64 +4,53 @@ using System.Security.Cryptography.X509Certificates;
 
 internal class Program
 {
-    static Dictionary<string, string> DictionaryApp = new();
-
-    private static void Main(string[] args)
+    static List<string> words = new List<string>();
+    private static string word;
+    
+    static void Main(string[] args)
     {
-        do
-        {
-            Console.WriteLine("\n\n=====================");
-            Console.WriteLine("Menu of the Dictionary app");
-            Console.WriteLine("Welcome to my app");
-            Console.WriteLine("1. Add");
-            Console.WriteLine("2. Delete");
-            Console.WriteLine("3. Edit");
-            Console.WriteLine("4. View");
-            Console.WriteLine("5. Search");
-            Console.WriteLine("Please enter an option:");
-            var option = Console.ReadLine();
-            // depending on the option, we will do different things.
-            if (option == "1")
+        Menu();
+    }            
+    public static void Menu()
+    {
             {
-
+                Console.WriteLine("\n\n=====================");
+                Console.WriteLine("Menu of the Dictionary");
+                Console.WriteLine("Welcome to my app");
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Delete");
+                Console.WriteLine("3. Edit");
+                Console.WriteLine("4. View");
+                Console.WriteLine("5. Search");
+                Console.WriteLine("Please enter an option:");
+                var option = Console.ReadLine();
+                // depending on the option, we will do different things.
+                if (option == "1")
+                {
+                    ShowAddScreen();
+                    Menu();
+                }
+                
+            
             }
-
-        } while (true);
     }
-
-    public static void SaveInformation(string newWord, string meaningOfTheWord)
-    {
-
-    }
-
     public static void ShowAddScreen()
     {
-        Console.WriteLine("[[ADDING A NEW WORD]]");
+        Console.WriteLine("===========================");
+        Console.WriteLine("[[ADDING A NEW WORD]]");    
         Console.WriteLine("Add a new word: ");
-        var newWord = Console.ReadLine();
+        string newWord = Console.ReadLine();
 
         Console.WriteLine("What's the meaning of the word: ");
-        var meaningOfTheWord = Console.ReadLine();
+        string meaningOfTheWord = Console.ReadLine();
 
-        DictionaryApp.Add(newWord, meaningOfTheWord);
-
-      
-       ( "eat": "to consume something solid or semi-solid, usually food through the mouth"),
-       ( "drink": "to consume liquid through the mouth" );
-
-        string eat = "none";
-        if (newWord. (eat, out eat))
-            (
-                Console.WriteLine("Found value: " + eat);
-        ShowAddScreen();
-        
-        
-        
-    }
-
+        string word = "NewWord =  " + newWord + "MeaningOfTheWord = " + meaningOfTheWord;
+        words.Add(word);
 
     }
 
+    
 
+   }
 
-
+}
